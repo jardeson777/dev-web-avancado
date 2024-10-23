9bce988f06dc83d14fce232f8a7755e0cfb74d69
Patrick Martins <patrick.martins@dataprev.gov.br>
2024-10-23 13:13:50 -0300

    feature/0001 - criado projeto spring inicial

---
+++ espaco_aluno/src/main/java/com/uff/espaco_aluno/EspacoAlunoApplication.java
@@ -0,0 +1,13 @@
+package com.uff.espaco_aluno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspacoAlunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspacoAlunoApplication.class, args);
+	}
+
+}
---
+++ espaco_aluno/src/test/java/com/uff/espaco_aluno/EspacoAlunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uff.espaco_aluno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspacoAlunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
